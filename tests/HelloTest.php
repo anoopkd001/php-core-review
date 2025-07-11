752f81b825d206934122d0b3bd0f0ecfc750508e
anoopkd001 <42534527+anoopkd001@users.noreply.github.com>
2025-07-11 18:15:33 +0530

    Add files via upload

---
+++ tests/HelloTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Hello;
+
+class HelloTest extends TestCase {
+    public function testGreet() {
+        $hello = new Hello();
+        $this->assertEquals("Hello, Anoop", $hello->greet("Anoop"));
+    }
+}
